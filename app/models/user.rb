6bda94f0cfec2b2ade7c312c4801eef69f9084c0
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-14 19:54:09 +0100

    Add friendship connection helper methods to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,23 @@ class User < ApplicationRecord
     has_many :buzzs, foreign_key: 'author_id', class_name: 'Buzz'
     has_many :followers, class_name: 'Following', foreign_key: 'followed_id'
     has_many :followed, class_name: 'Following', foreign_key: 'follower_id'
+
+    def follow(user)
+        followed.create(followed_id: user.id) unless is_following?(user)
+    end
+
+    def unfollow(user)
+        f = followed.where(followed_id: user.id).first
+        f.destroy unless f.nil?
+    end
+
+    def is_following?(user)
+        return false if user.id.nil?
+        !followed.where(followed_id: user.id).first.nil?
+    end
+
+    def is_followed_by?(user)
+        return false if user.id.nil?
+        !followers.where(follower_id: user.id).first.nil?
+    end
 end
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -7,24 +7,25 @@ RSpec.describe Following, type: :model do
       @userB = User.create(username: 'banana', fullname: 'Banana Guava')
       @userC = User.create(username: 'cashew', fullname: 'Cashew Paw-paw')
 
-      @f1 = @userA.followers.create(follower_id: @userB.id)
-      @f2 = @userA.followers.create(follower_id: @userC.id)
+      @userB.follow(@userA)
+      @userC.follow(@userA)
     end
 
     it "return 2 followers for 'userA'" do
       expect(@userA.followers.count).to eq(2)
     end
 
-    it 'returns userB as the first in the follower' do
-      expect(@userA.followers.first.follower).to eq(@userB)
+    it 'returns userB is following userA' do
+      expect(@userB.is_following?(@userA)).to be true
     end
 
-    it 'returns the 1 following for @userB' do
-      expect(@userB.followed.count).to eq(1)
+    it 'returns userA is followed by userC' do
+      expect(@userA.is_followed_by?(@userC)).to be true
     end
 
-    it 'returns the userA as the first in userB following list' do
-      expect(@userB.followed.first.followed).to eq(@userA)
+    it 'returns the userB unfollows userA' do
+      @userB.unfollow(@userA)
+      expect(@userB.is_following?(@userA)).to be false
     end
   end
 end
