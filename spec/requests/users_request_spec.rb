7a313954dc2173a4e481f9ac8c07109de8d6f561
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-15 20:13:34 +0100

    Refactor requests spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users management", type: :request do
+RSpec.describe "Users", type: :request do
     it "signs up User and redirects to the buzz#index's page" do
         get "/users/new"
         expect(response).to render_template(:new)
@@ -10,21 +10,6 @@ RSpec.describe "Users management", type: :request do
         expect(response).to redirect_to(buzzs_url)
         follow_redirect!
     
-        expect(response).to render_template("buzzs/index")
-        expect(response.body).to include("TWEETS")
-        expect(response.body).to include("WHO TO FOLLOW")
-        expect(response.body).to include("BELLO BABAKOLO")
-    end
-
-    it "signs in User and redirects to the buzz#index's page" do
-        get "/sessions/new"
-        expect(response).to render_template(:new)
-
-        post "/sessions", :params => { :user => {:username => "exboybeekay"} }
-
-        expect(response).to redirect_to(buzzs_url)
-        follow_redirect!
-
         expect(response).to render_template("buzzs/index")
         expect(response.body).to include("TWEETS")
         expect(response.body).to include("WHO TO FOLLOW")
