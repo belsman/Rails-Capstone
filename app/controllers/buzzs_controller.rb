191adcd5970f3074965e29519e45f3e01b139f85
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-16 11:49:17 +0100

    Spec Buzz controller

--- app/controllers/buzzs_controller.rb
+++ app/controllers/buzzs_controller.rb
@@ -1,6 +1,19 @@
 class BuzzsController < ApplicationController
+    include BuzzsHelper
 
     def index
-        
+        @new_buzz = Buzz.new
+        @timeline_buzzs = current_user.buzzs
+    end
+
+    def create
+        @buzz = current_user.buzzs.new(buzzs_params)
+
+        if @buzz.save
+          redirect_to buzzs_path, notice: 'Buzz was successfully created.'
+        else
+            @timeline_buzzs = current_user.buzzs
+            render :index, alert: 'Buzz was not created.'
+        end
     end
 end
--- app/helpers/buzzs_helper.rb
+++ app/helpers/buzzs_helper.rb
@@ -1,2 +1,5 @@
 module BuzzsHelper
+    def buzzs_params
+        params.require(:buzz).permit(:text)
+    end
 end
