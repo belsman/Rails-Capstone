a35999abc134d53cb75461e6df17dd96648185f1
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-16 22:47:53 +0100

    Add style to login and registration page

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'bootstrap', '~> 5.0.0.alpha3'
+
+gem 'jquery-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
